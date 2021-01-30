6e49555a9c8a753e4461b0135dccf83f8fa7e711
fferrari <f.ferrari@wanadoo.fr>
2021-01-30 16:21:01 +0100

    Handling failure response on addBatchSpecification

--- src/main/scala/com/fferrari/pricescraper/service/PriceScraperServiceImpl.scala
+++ src/main/scala/com/fferrari/pricescraper/service/PriceScraperServiceImpl.scala
@@ -1,6 +1,6 @@
 package com.fferrari.pricescraper.service
 
-import akka.actor.typed.ActorRef
+import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import com.fferrari.pricescraper.PriceScraper.Command
@@ -9,10 +9,18 @@ import org.slf4j.LoggerFactory
 
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
+import java.util.concurrent.TimeoutException
+
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.grpc.GrpcServiceException
+import io.grpc.Status
 
 class PriceScraperServiceImpl(batchManager: ActorRef[BatchManagerEntity.Command],
                               context: ActorContext[Command]) extends PriceScraperService {
   private val logger = LoggerFactory.getLogger(getClass)
+
+  // private val sharding = ClusterSharding(system)
+  implicit val executionContext = context.executionContext
 
   override def addBatchSpecification(in: AddBatchSpecificationRequest): Future[BatchSpecification] = {
     logger.info("addBatchSpecification {}", in.name)
@@ -25,6 +33,16 @@ class PriceScraperServiceImpl(batchManager: ActorRef[BatchManagerEntity.Command]
           in.provider,
           in.intervalSeconds, _)
       )(3.seconds, context.system.scheduler)
-      .map(batchSpecificationId => BatchSpecification(batchSpecificationId.toString))(context.executionContext)
+      .map(batchSpecificationId => BatchSpecification(batchSpecificationId.toString))
+      .recoverWith {
+        case _: TimeoutException =>
+          Future.failed(
+            new GrpcServiceException(Status.UNAVAILABLE.withDescription("AddBatchSpecification has timed out"))
+          )
+        case ex =>
+          Future.failed(
+            new GrpcServiceException(Status.INVALID_ARGUMENT.withDescription(ex.getMessage))
+          )
+      }
   }
 }
